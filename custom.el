5d6c5ae7e43024796b905303752746f3cd6f765d
Chao <chao.jin75618@gmail.com>
2017-07-16 11:33:31 -0700

    add custom.el

---
+++ custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes (quote (hc-zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" default)))
+ '(display-time-mode t)
+ '(package-selected-packages
+   (quote
+    (hc-zenburn-theme anti-zenburn-theme zenburn-theme color-theme-sanityinc-solarized auctex python-mode evil undo-tree page-break-lines expand-region hlinum diminish help-fns+ ac-math pos-tip auto-complete fuzzy helm-gtags helm-projectile helm-ag helm exec-path-from-shell)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Inconsolata" :foundry "PfEd" :slant normal :weight bold :height 143 :width normal)))))
