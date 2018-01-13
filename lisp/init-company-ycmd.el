0276cba7859797e8c546d96ef6ffd7bfda43440c
Chao <chao.jin75618@gmail.com>
2018-01-13 14:36:35 -0800

    update ycmd for python

--- lisp/init-company-ycmd.el
+++ lisp/init-company-ycmd.el
@@ -4,11 +4,11 @@
 (company-ycmd-setup)
 (set-variable 'ycmd-server-command '("python" "/home/jinchao/.vim/bundle/YouCompleteMe/third_party/ycmd/ycmd/"))
 
-;; (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-;; (setq company-backends (delete 'company-semantic company-backends))
-;; (eval-after-load 'company
-;;   '(add-to-list
-;;     'company-backends 'company-ycmd))
+(setq company-backends (delete 'company-semantic company-backends))
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends 'company-ycmd))
+
 (defun my-ycmd-common-hook()
   (company-mode t)
   (ycmd-mode t))
@@ -17,5 +17,6 @@
 (add-hook 'c-mode-hook 'my-ycmd-common-hook)
 (add-hook 'objc-mode-hook 'my-ycmd-common-hook)
 (add-hook 'emacs-lisp-mode-hook 'my-ycmd-common-hook)
+(add-hook 'python-mode-hook 'my-ycmd-common-hook)
 
 (provide 'init-company-ycmd)
