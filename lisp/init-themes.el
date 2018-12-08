3ceb7d2631cedce1ce86e4f5425c270becd07386
Chao <chao.jin75618@gmail.com>
2018-12-08 13:51:14 -0800

    update theme config, add tmux.conf

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -4,30 +4,24 @@
 (require-package 'hc-zenburn-theme)
 ;(require-package 'color-theme-sanityinc-tomorrow)
 ;(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/color-theme-sanityinc-solarized-20160429.1903")
-;; if you don't customize it, this is the theme you get
-;(setq-default custom-enabled-themes '(sanityinc-solarized-dark))
-(setq-default custom-enabled-themes '(hc-zenburn))
-;(setq-default custom-enabled-themes '(sanitynic-tomorrow-bright))
+;; themes list, 1st is for graphic, 2nd is for terminal
+(setq-default custom-enabled-themes '(hc-zenburn sanityinc-solarized-dark))
 
 ;; Ensure that themes will be applied even if they have not been customized
-;; (if (display-graphic-p)
-;;     ;; (enable-theme '(hc-zenburn))
-;;     (enable-theme 'sanityinc-solarized-dark)
-;;   (enable-theme '(sanityinc-solarized-dark)))
-;; (defun reapply-themes ()
-;;   "Forcibly load the themes listed in `custom-enabled-themes'."
-;;   (dolist (theme custom-enabled-themes)
-;;     (unless (custom-theme-p theme)
-;;       (load-theme theme)))
-;;   (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
-
 (defun reapply-themes ()
+  "Forcibly load the themes listed in `custom-enabled-themes'."
+  (dolist (theme custom-enabled-themes)
+    (unless (custom-theme-p theme)
+      (load-theme theme)))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+
+(defun reapply-themes-new ()
   ;; different theme for terminal and graphic
   (if (display-graphic-p)
-      (load-theme 'hc-zenburn)
-    (load-theme 'sanityinc-solarized-dark)))
+      (load-theme (nth 0 custom-enabled-themes))
+    (load-theme (nth 1 custom-enabled-themes))))
 
-(add-hook 'after-init-hook 'reapply-themes)
+(add-hook 'after-init-hook 'reapply-themes-new)
 
 ;; ;; Toggle between light and dark
 ;; (defun light ()
