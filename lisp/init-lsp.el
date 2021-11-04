935e4d8ba8505b31ff55cf1fcdeae22048f19b5e
Chao <chao.jin75618@gmail.com>
2021-11-04 11:53:33 -0700

    updated configurations

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -26,5 +26,54 @@
 (require 'lsp-mode)
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
+(add-hook 'python-mode 'lsp)
+
+(require-package 'lsp-ui)
+(add-hook 'lsp 'lsp-ui-mode)
+(setq lsp-ui-doc-position 'bottom)
+
+(setq lsp-ui-doc-enable t
+      lsp-ui-peek-enable t
+      lsp-ui-sideline-enable t
+      lsp-ui-imenu-enable t
+      lsp-ui-flycheck-enable t)
+
+;; LSP
+;; (use-package lsp-mode
+;;   :init
+;;   (setq lsp-keymap-prefix "C-c l")
+;;   :hook ((python-mode . lsp-deferred)
+;;          (go-mode . lsp-deferred)
+;;          (rust-mode . lsp-deferred)
+;;          (typescript-mode . lsp-deferred)
+;;          (lsp-mode . lsp-enable-which-key-integration))
+;;   :commands (lsp lsp-deferred))
+
+;; (use-package lsp-ui
+;;   :hook (lsp-mode . lsp-ui-mode)
+;;   :custom
+;;   (lsp-ui-doc-position 'bottom))
+
+;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+
+;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; (use-package company-lsp
+;;   :commands company-lsp)
+
+;; ;;Optional - provides snippet support.
+
+;; (use-package yasnippet
+;;   :commands yas-minor-mode
+;;   :hook (
+;;          (go-mode . yas-minor-mode)
+;;          (python-mode . yas-minor-mode)
+;;          ))
+
+;; (setq lsp-ui-doc-enable t
+;;       lsp-ui-peek-enable t
+;;       lsp-ui-sideline-enable t
+;;       lsp-ui-imenu-enable t
+;;       lsp-ui-flycheck-enable t)
 
 (provide 'init-lsp)
