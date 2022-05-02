384dd874886cc6e2ca702ccec245cae3fc58e08f
Chao <chao.jin75618@gmail.com>
2022-05-02 14:32:26 -0700

    disable undo tree auto save file

--- lisp/editing-utils/init-undo-tree.el
+++ lisp/editing-utils/init-undo-tree.el
@@ -2,5 +2,7 @@
 ;; (require 'undo-tree-autoloads)
 (global-set-key "\C-xu" 'undo-tree-visualize)
 (global-undo-tree-mode t)
+(setq undo-tree-auto-save-history nil)
 
 (provide 'init-undo-tree)
+;;; init-undo-tree.el ends here
