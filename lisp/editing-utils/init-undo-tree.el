031e5f9cfd1616678635b0d0477e53c2d4e3a239
Chao <chao.jin75618@gmail.com>
2026-03-28 13:39:22 -0700

    fixed typo

--- lisp/editing-utils/init-undo-tree.el
+++ lisp/editing-utils/init-undo-tree.el
@@ -3,8 +3,8 @@
 (global-undo-tree-mode t)
 (setq undo-tree-auto-save-history nil)
 
-;; Fix: Ensure undo-tree properly tracks saved state interminal mode
-(setq undo-tree-enable-undo-in-regin nil)
+;; Fix: Ensure undo-tree properly tracks saved state in terminal mode
+(setq undo-tree-enable-undo-in-region nil)
 
 (provide 'init-undo-tree)
 ;;; init-undo-tree.el ends here
